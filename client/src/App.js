ad624dcbb4a4b15eb19159aba6670269931e2f8d
BuiThucNam-20200857 <nam.bt200857@sis.hust.edu.vn>
2023-01-07 22:54:08 +0700

    update frontEnd react

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Match from './pages/Match/Match'
+import MatchDetail from './pages/Match/MatchDetail/MatchDetail'
+import MatchChart from './pages/MatchChart/MatchChart'
+import News from './pages/News/News'
+import Player from './pages/Players/Player'
+import Ranking from './pages/Rankings/Rangking'
+import Stat from './pages/Stats/Stat'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/match' element={<Match />}/>
+        <Route path='/matchDetail'element={<MatchDetail />}/>
+        <Route path='/matchChart' element={<MatchChart />}/>
+        <Route path='/news' element={<News />}/>
+        <Route path='/player' element={<Player />}/>
+        <Route path='/ranking' element={<Ranking />}/>
+        <Route path='/stat' element={<Stat />}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Match/MatchDetail/MatchDetail.jsx
@@ -0,0 +1,15 @@
+import './MatchDetail.css'
+import Header from '../../../components/Header/Header'
+import Footer from '../../../components/Footer/Footer'
+
+const MatchDetail = () => {
+    return (
+        <div className='matchDetail'>
+            <Header />
+            <h1 className='matchDetailh1'>Match DeTail</h1>
+            <Footer />
+        </div>
+    )
+}
+
+export default MatchDetail
