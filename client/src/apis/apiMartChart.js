b438c8fc05abcc719193b6c8bc9d55b68415a377
@nhTuan <tuan.na194201@sis.hust.edu.vn>
2023-02-09 11:20:59 +0700

    Nguyen Anh Tuan update Sơ đồ thi đấu

---
+++ client/src/apis/apiMartChart.js
@@ -0,0 +1,369 @@
+export const Bracket = [
+
+    //CK
+    {
+      id: 19753,
+      nextMatchId: null,
+      tournamentRoundText: "4",
+      startTime: "2021-05-30",
+      state: "SCHEDULED",
+      participants: [
+        {
+            id: "14754a1a-932c-4992-8dec-f7f94a339960",
+            resultText: "3(4)" ,
+            isWinner: true,
+            status: null,
+            name: "ARG",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "14754a1a-932c-4992-8dec-f7f94a339960",
+            resultText: "3(2)",
+            isWinner: false,
+            status: null,
+            name: "FRA",
+            picture: "teamlogos/client_team_default_logo"
+          }
+      ]
+    },
+
+
+    // BK2
+    {
+      id: 19754,
+      nextMatchId: 19753,
+      tournamentRoundText: "3",
+      startTime: "2021-05-30",
+      state: "SCHEDULED",
+      participants: [
+        {
+          id: "14754a1a-932c-4992-8dec-f7f94a339960",
+          resultText: null,
+          isWinner: false,
+          status: null,
+          name: "CoKe BoYz",
+          picture: "teamlogos/client_team_default_logo"
+        }
+      ]
+    },
+
+    //Tk3
+    {
+      id: 19755,
+      nextMatchId: 19754,
+      tournamentRoundText: "2",
+      startTime: "2021-05-30",
+      state: "SCORE_DONE",
+      participants: [
+        {
+          id: "14754a1a-932c-4992-8dec-f7f94a339960",
+          resultText: "Won",
+          isWinner: true,
+          status: "PLAYED",
+          name: "CoKe BoYz",
+          picture: "teamlogos/client_team_default_logo"
+        },
+        {
+          id: "d16315d4-7f2d-427b-ae75-63a1ae82c0a8",
+          resultText: "Lost",
+          isWinner: false,
+          status: "PLAYED",
+          name: "Aids Team",
+          picture: "teamlogos/client_team_default_logo"
+        }
+      ]
+    },
+
+    //Vong 16 3
+    {
+        id: 19764,
+        nextMatchId: 19755,
+        tournamentRoundText: "1",
+        startTime: "2021-05-30",
+        state: "SCHEDULED",
+        participants: [
+          {
+            id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "team 5",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "team 6",
+            picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+          }
+        ]
+      },
+      {
+        id: 19765,
+        nextMatchId: 19755,
+        tournamentRoundText: "1",
+        startTime: "2021-05-30",
+        state: "SCHEDULED",
+        participants: [
+          {
+            id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "BLUEJAYS",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "Bosphorus",
+            picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+          }
+        ]
+      },
+    //
+
+
+    //TK 4
+    {
+      id: 19756,
+      nextMatchId: 19754,
+      tournamentRoundText: "2",
+      startTime: "2021-05-30",
+      state: "RUNNING",
+      participants: [
+        {
+          id: "d8b9f00a-0ffa-4527-8316-da701894768e",
+          resultText: null,
+          isWinner: false,
+          status: null,
+          name: "Art of kill",
+          picture: "teamlogos/client_team_default_logo"
+        }
+      ]
+    },
+
+    //Vong 16 4
+    {
+        id: 19766,
+        nextMatchId: 19756,
+        tournamentRoundText: "1",
+        startTime: "2021-05-30",
+        state: "SCHEDULED",
+        participants: [
+          {
+            id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "team 7",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "team 8",
+            picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+          }
+        ]
+      },
+      {
+        id: 19767,
+        nextMatchId: 19756,
+        tournamentRoundText: "1",
+        startTime: "2021-05-30",
+        state: "SCHEDULED",
+        participants: [
+          {
+            id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "BLUEJAYS",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "Bosphorus",
+            picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+          }
+        ]
+      },
+
+    //
+    //
+
+//
+
+
+    //BK 1
+    {
+      id: 19757,
+      nextMatchId: 19753,
+      tournamentRoundText: "3",
+      startTime: "2021-05-30",
+      state: "SCHEDULED",
+      participants: []
+    },
+
+    // TK1
+    {
+      id: 19758,
+      nextMatchId: 19757,
+      tournamentRoundText: "2",
+      startTime: "2021-05-30",
+      state: "SCHEDULED",
+      participants: [
+        {
+          id: "9397971f-4b2f-44eb-a094-722eb286c59b",
+          resultText: null,
+          isWinner: false,
+          status: null,
+          name: "Crazy Pepes",
+          picture: "teamlogos/client_team_default_logo"
+        }
+      ]
+    },
+    // vong 16 1
+    {
+        id: 19760,
+        nextMatchId: 19758,
+        tournamentRoundText: "1",
+        startTime: "2021-05-30",
+        state: "SCHEDULED",
+        participants: [
+          {
+            id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: " team 1",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: " team 2",
+            picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+          }
+        ]
+      },
+      {
+        id: 19761,
+        nextMatchId: 19758,
+        tournamentRoundText: "1",
+        startTime: "2021-05-30",
+        state: "SCHEDULED",
+        participants: [
+          {
+            id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "BLUEJAYS",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "Bosphorus",
+            picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+          }
+        ]
+      },
+    //
+    //
+    // TK2
+    {
+      id: 19759,
+      nextMatchId: 19757,
+      tournamentRoundText: "2",
+      startTime: "2021-05-30",
+      state: "SCHEDULED",
+      participants: [
+        {
+          id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+          resultText: null,
+          isWinner: false,
+          status: null,
+          name: "BLUEJAYS",
+          picture: "teamlogos/client_team_default_logo"
+        },
+        {
+          id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+          resultText: null,
+          isWinner: false,
+          status: null,
+          name: "Bosphorus",
+          picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+        }
+      ]
+    },
+    //vong 16 2
+    {
+        id: 19762,
+        nextMatchId: 19759,
+        tournamentRoundText: "1",
+        startTime: "2021-05-30",
+        state: "SCHEDULED",
+        participants: [
+          {
+            id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "team 3",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "team 4",
+            picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+          }
+        ]
+      },
+
+      {
+        id: 19763,
+        nextMatchId: 19759,
+        tournamentRoundText: "1",
+        startTime: "2021-05-30",
+        state: "SCHEDULED",
+        participants: [
+          {
+            id: "42fecd89-dc83-4821-80d3-718acb50a30c",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "BLUEJAYS",
+            picture: "teamlogos/client_team_default_logo"
+          },
+          {
+            id: "df01fe2c-18db-4190-9f9e-aa63364128fe",
+            resultText: null,
+            isWinner: false,
+            status: null,
+            name: "Bosphorus",
+            picture: "teamlogos/r7zn4gr8eajivapvjyzd"
+          }
+        ]
+      },
+    //
+    //
+];
