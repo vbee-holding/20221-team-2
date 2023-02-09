b2fa79a17fc48446214a64c73c7af06f0cd54037
BuiThucNam-20200857 <nam.bt200857@sis.hust.edu.vn>
2023-02-09 18:03:51 +0700

    Update version2 NewsPage

---
+++ client/src/components/Contents/ContentNews/ContentNews.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './ContentNews.css'
+
+const ContentNews = () => {
+    const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('http://localhost:3000/NewsData.json')
+    //     .then(response => {
+    //         return response.json()
+    //     })
+    //     .then(data => {
+    //         setData(data)
+    //     })
+    //     .catch(error => {
+    //         console.error(error)
+    //     })
+    // }, [])
+    useEffect(() => {
+        async function fetchData() {
+          const result = await axios.get('./NewsData.json');
+          console.log(result.data)
+          setData(result.data);
+        }
+        fetchData();
+    }, []);
+    
+
+    return(
+        <div className='cNews'>
+            {data.map(element => (
+                <a className='cNewsLink' key={element.link} href={element.link}>
+                    <div>
+                        <div className='cNewsTitle'>{element.title}</div>
+                        <div className='cNewsTime'>{element.newspaper} - {element.time}</div>
+                    </div>
+                    <img className='cNewsImg' src={element.img} alt="anh" />
+                </a>
+            ))}
+        </div>
+    )
+}
+
+export default ContentNews
