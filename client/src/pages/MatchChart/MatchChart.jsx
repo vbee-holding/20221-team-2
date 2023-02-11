f568c688db4e0076b7634eeede5aca804e9560cc
@nhTuan <tuan.na194201@sis.hust.edu.vn>
2023-02-11 14:41:08 +0700

    Nguyen Anh Tuan update sơ đồ thi đấu

--- client/src/pages/MatchChart/MatchChart.jsx
+++ client/src/pages/MatchChart/MatchChart.jsx
@@ -1,13 +1,13 @@
 import './MatchChart.css'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import App from '../../components/Contents/MarchChart/MartChart'
+import MartChart from '../../components/Contents/MarchChart/MartChart'
 
 const MatchChart = () => {
     return (
         <div>
             <Header />
-               <App/>
+               <MartChart/>
             <Footer />
         </div>
     )
