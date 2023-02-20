0d0c866496cd3e2ff666b63e73747c5f373f0335
racoon <hai.tranduc4270@gmail.com>
2023-02-20 18:58:40 +0700

    LeQuocAnh-20204509-codeCauThu-ThongKe

--- client/src/pages/Stats/Stat.jsx
+++ client/src/pages/Stats/Stat.jsx
@@ -1,3 +1,4 @@
+import {statData} from './StatData'
 import './Stat.css'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
@@ -6,7 +7,40 @@ const Stat = () => {
     return (
         <div>
             <Header />
-            <h1 className='stat'>Số liệu thống kê</h1>
+            <div className='Statistics'>
+                {Object?.values(statData).map((element, index) => {
+                    return(
+                        <div className='loopStat'>
+                            <div className='loopStat1'>
+                                <div className='opening1'>{element[0].category}</div>
+                                <div className='opening2'>
+                                    <div>Player</div>
+                                    <div className='category2'>{element[0].category}</div>
+                                </div>
+                            </div>
+                            
+                            <div className='loopStat2'>
+                                {element.map((child) => (
+                                    <a href={child.information}>
+                                        <div className='body'>
+                                            <div className='bodyRank'>{child.rankNumber}</div>
+                                            <img className='bodyImage' src={child.image} />
+                                            <div className='bodyNation'>
+                                                <div className='name'>{child.name}</div>
+                                                <div className='nation'>
+                                                    <img className='nationImage' src ={child.nationImage} />
+                                                    <div className='nationName'>{child.nationName}</div>
+                                                </div>
+                                            </div>
+                                            <div className='bodyGoals'>{child.quantity}</div>
+                                        </div>
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
             <Footer />
         </div>
     )
